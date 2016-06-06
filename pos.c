f8e376fdec0ade4fc51744d5dfeb9189a7d1e458
GloryAlfonce <ggloryalfonce@gmail.com>
2016-06-06 09:20:34 +0530

    Update and rename .gitignore to pos.c

---
+++ pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n=0)
+printf("Zero\n);
+else if(n>0)
+printf("Positive number\n");
+else
+printf("negative number\n");
+getch();
+}
